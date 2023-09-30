0ac58f2372a84e57c8cc549c3629380e1562477b
Santhosh <santhoshjeeva440@gmail.com>
2023-09-30 22:33:21 +0530

    edit yml file

--- resources/configuration/environment.py
+++ resources/configuration/environment.py
@@ -5,7 +5,7 @@ class qa:
 
     browser = "chrome"
     url = "http://adactinhotelapp.com/"
-    username = "Autoboys"
+    username = "Auto1Boyz"
     password = "Testing@12345"
     test_date_path = "/Users/santhosh/Documents/robotframework_selenium/test_data/qa_test_data.csv"
 
