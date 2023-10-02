070e9ab00d8ec2ce0c97cc87634c2ed4ca2ae3d9
Santhosh <santhoshjeeva440@gmail.com>
2023-10-02 17:03:51 +0530

    final ci/cd edit

--- install_libraries.sh
+++ install_libraries.sh
@@ -4,4 +4,4 @@ echo "Installing requirements"
 chmod 0755 resources/configuration/requirements.txt
 python3 -m pip install -r resources/configuration/requirements.txt
 python3 -m pip install webdrivermanager
-python3 -m pip install webdrivermanager firefox chrome
+webdrivermanager firefox chrome
